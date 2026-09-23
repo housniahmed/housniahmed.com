c6b2d3bb383ee995293de7593aef0f8a2a857f32
Ahmed EL HOUSNI <housniforbusiness@gmail.com>
2026-09-23 11:57:16 +0100

    fix: use relative component imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { SiteFooter } from "@/components/site-footer";
-import { SiteHeader } from "@/components/site-header";
+import { SiteFooter } from "../components/site-footer";
+import { SiteHeader } from "../components/site-header";
 
 const capabilities = [
   {
